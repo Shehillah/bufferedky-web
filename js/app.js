8ca7eeee4ddc9591f93dcbd2ffe5bc1b1cb54522
SHEHI <hussainiabubakar42@gmail.com>
2024-04-27 16:57:34 +0100

    nayi wa slider hannaye

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ $('#hero-slider').owlCarousel({
     margin:0,
     nav:true,
     items:1,
+    smartSpeed: 1000,
+    dots:false,
+    navText:['PREV', 'NEXT'],
     responsive:{
         0:{
         
