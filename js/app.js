9ae6a1dc7f5b9eab683d73fbbbe7a89dbbedc1ca
SHEHI <hussainiabubakar42@gmail.com>
2024-04-30 18:38:34 +0100

    na gaji zan tafi

--- js/app.js
+++ js/app.js
@@ -47,3 +47,11 @@ $(document).ready(function(){
         }
     })
     });
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots:true,
+        items:1,
+    })
